1cb5326fb62b3e4da71efed81c17bd059b3bc9ae
Matthew Vallance <matthew.vallance@outlook.com>
2024-01-28 17:18:30 +0000

    File setup

---
+++ client_server_communication/server/Database.java
@@ -0,0 +1,7 @@
+package client_server_communication.server;
+
+public class Database {
+    public void db_connection() {
+        System.out.println("Test");
+    }
+}
---
+++ client_server_communication/server/main.java
@@ -0,0 +1,12 @@
+package client_server_communication.server;
+
+public class main {
+    public static void main(String[] args) {
+        // Connect DB
+        Database db = new Database();
+        db.db_connection();
+
+
+    }
+
+}
