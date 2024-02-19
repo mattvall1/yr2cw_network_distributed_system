2461e563deff5fa50c219bb10fb2d871dbb6741d
Matthew Vallance <matthew.vallance@outlook.com>
2024-02-19 16:28:09 +0000

    Added TODOs, commented out ID retrival (for the moment)

--- client_server_communication/server/Server.java
+++ client_server_communication/server/Server.java
@@ -5,18 +5,21 @@ import java.net.*;
 public class Server {
     public static void main(String[] args) throws Exception {
         // Create server socket
-        ServerSocket serverSocket = new ServerSocket(850);
+        try (ServerSocket serverSocket = new ServerSocket(850)) {
 
-        // Server executes continuously
-        while (true) {
-            // Connect to client socket
-            Socket socket = serverSocket.accept();
+            // Server executes continuously
+            while (true) {
+                // Connect to client socket
+                Socket socket = serverSocket.accept();
 
-            // Handle client connection in a new thread
-            new Thread(new ClientHandler(socket)).start();
+                // Handle client connection in a new thread
+                new Thread(new ClientHandler(socket)).start();
 
-            // If we get to this point, a client is successfully connected
-            System.out.println("IP: " + socket.getLocalAddress() + ", at port: " + socket.getLocalPort() + " connected");
+                // If we get to this point, a client is successfully connected
+                System.out.println("IP: " + socket.getLocalAddress() + ", at port: " + socket.getLocalPort() + " connected");
+            }
+        } catch (IOException err) {
+            System.out.println("Error:" + err);
         }
     }
 
@@ -35,18 +38,21 @@ public class Server {
 
 
                 int userID = Integer.parseInt(bufferedReader.readLine());
-                // TODO: Check DB for user details, make sure ID is retrieved correctly
-                if(userID == 666) {
-                    System.out.println("ID successfully retrieved");
-//                    bufferedReader.readLine() = "\n";
-                } else {
+                System.out.println(userID);
 
-                }
+//                // TODO: Check DB for user details, make sure ID is retrieved correctly
+//                if(userID == 666) {
+//                    System.out.println("ID successfully retrieved");
+////                    bufferedReader.readLine() = "\n";
+//                } else {
+//
+//                }
 
                 // Define received data string
                 String receivedData;
 
                 // Read from client + print received text (this is always looping)
+                // TODO: Send data back to ALL clients (simple GC feature)
                 while ((receivedData = bufferedReader.readLine()) != null) {
                     System.out.println(receivedData);
                 }
