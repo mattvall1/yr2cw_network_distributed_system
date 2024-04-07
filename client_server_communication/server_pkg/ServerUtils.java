801ef504049664bee7faac390932cc35f680e92d
Matthew Vallance <matthew.vallance@outlook.com>
2024-04-07 13:06:17 +0100

    New server datetime function, add to logging

--- client_server_communication/server_pkg/ServerUtils.java
+++ client_server_communication/server_pkg/ServerUtils.java
@@ -2,33 +2,37 @@ package client_server_communication.server_pkg;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
 public class ServerUtils {
     // Console output routine for server logging
     public static void console_output(Integer sender_id, String message) {
+        // Create datetime string to put on every output
+        String current_time = get_current_datetime() + " | ";
         if (message.matches("^brm-.*")) {
             // Remove regex from string
             message = message.replaceFirst("^brm-", "");
 
             // Print to console
-            System.out.println("Broadcast from " + sender_id + ": " + message);
+            System.out.println(current_time + "Broadcast from " + sender_id + ": " + message);
         } else if (message.matches("^dm-.*")) {
             // Get index of first underscore after user ID, to split message properly
             int next_dash = message.indexOf("-", 3);
             int receiver = Integer.parseInt(message.substring(3, next_dash));
 
             // Print to console
-            System.out.println("Direct message from " + sender_id + " to " + receiver + ":" + message.substring(next_dash + 1));
+            System.out.println(current_time + "Direct message from " + sender_id + " to " + receiver + ": " + message.substring(next_dash + 1));
         } else if(message.matches("^exit$")) {
-            System.out.println("User " + sender_id + " will now be disconnected...");
+            System.out.println(current_time + "User " + sender_id + " will now be disconnected...");
         } else if(message.matches("^act-grp-details$")) {
-            System.out.println("Coordinator requested active group details");
+            System.out.println(current_time + "Coordinator requested active group details");
         } else if(message.matches("^grp-details$")) {
-            System.out.println("User " + sender_id + " requested group details");
+            System.out.println(current_time + "User " + sender_id + " requested group details");
         } else {
             // This code shouldn't run, keep this for potential debugging
-            System.out.println("RECEIVED MESSAGE: " + message);
+            System.out.println(current_time + "RECEIVED MESSAGE: " + message);
         }
     }
 
@@ -43,6 +47,13 @@ public class ServerUtils {
 
         // If this user is the coordinator, tell them.
         if(is_coordinator) ServerMessagingHelper.send_new_coordinator_info(user_id);
+    }
+
+    public static String get_current_datetime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
+
+        return now.format(formatter);
     }
 
 }
