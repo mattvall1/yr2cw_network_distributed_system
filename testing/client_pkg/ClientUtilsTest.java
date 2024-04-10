ecbbb849126cfff9d90f0878211a4c7c3cdfcf70
Matthew Vallance <matthew.vallance@outlook.com>
2024-04-10 19:36:00 +0100

    ClientUitlsTesting passed

---
+++ testing/client_pkg/ClientUtilsTest.java
@@ -0,0 +1,46 @@
+package testing.client_pkg;
+
+import client_server_communication.client_pkg.ClientUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+public class ClientUtilsTest {
+
+    @Test
+    public void test_initial_menu() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        ClientUtils.initial_menu(1);
+
+        String expectedOutput  = "Welcome 1! Please read the following information before continuing\n" +
+                "To send a broadcast message, start with: 'brm-'...\n" +
+                "To send a direct message, start with: 'dm-USERIDHERE-'...\n" +
+                "To get details of all group members, enter 'grp-details'\n" +
+                "To exit/leave the chat program, press ctrl+C\n";
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    public void test_get_current_datetime() {
+        String currentDatetime = ClientUtils.get_current_datetime();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        String expectedDatetime = now.format(formatter);
+
+        assertTrue(currentDatetime.startsWith(expectedDatetime.substring(0, expectedDatetime.length() - 2)));
+    }
+}
