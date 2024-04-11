58f92bb53f7a5b34459870a535165b29d62a45dc
Matthew Vallance <matthew.vallance@outlook.com>
2024-04-11 15:38:29 +0100

    ip/port verification

--- client_server_communication/Client.java
+++ client_server_communication/Client.java
@@ -11,10 +11,23 @@ public class Client {
         try {
             Scanner scan = new Scanner(System.in);
 
-            System.out.println("Enter IP: ");
-            String user_ip = scan.nextLine();
-            System.out.println("Enter port:");
-            String user_port = scan.nextLine();
+            // Check the user enters the server ip address '127.0.0.1' and port '850'
+            String user_ip = "";
+            String user_port = "";
+            while (!user_ip.equals("127.0.0.1")) {
+                System.out.println("Enter IP: ");
+                user_ip = scan.nextLine();
+                if(!user_ip.equals("127.0.0.1")) {
+                    System.out.println("That was incorrect, did you input the correct IP?");
+                }
+            }
+            while (!user_port.equals("850")) {
+                System.out.println("Enter port: ");
+                user_port = scan.nextLine();
+                if(!user_port.equals("850")) {
+                    System.out.println("That was incorrect, did you input the correct port?");
+                }
+            }
 
 
             Socket client_socket = new Socket(user_ip, Integer.parseInt(user_port));
